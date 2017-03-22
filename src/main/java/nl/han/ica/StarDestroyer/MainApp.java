6d0181b1496663c9fb1ee00050b57d5477af94e6
Marijn Besselking <njirambem@gmail.com>
2017-03-22 21:12:46 +0100

    trying to add shit

--- src/main/java/nl/han/ica/StarDestroyer/MainApp.java
+++ src/main/java/nl/han/ica/StarDestroyer/MainApp.java
@@ -1,10 +1,13 @@
 package nl.han.ica.StarDestroyer;
 
+import nl.han.ica.OOPDProcessingEngineHAN.Engine.GameEngine;
+import processing.core.PApplet;
+
 /**
  * Created by Marijn Besseling on 22-Mar-17.
  */
-public class MainApp {
+public class MainApp extends GameEngine {
     public static void main(String[] args) {
-
+        PApplet.main(new String[]{"nl.han.ica.StarDestroyer.MainApp"});
     }
 }
