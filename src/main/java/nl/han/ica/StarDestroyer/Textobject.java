45fec9446d82cac2f26389ef92942e76c59373f6
Hendrik <Hendrik4king@hotmail.com>
2017-03-24 12:25:03 +0100

    Added some body to Textobject

--- src/main/java/nl/han/ica/StarDestroyer/Textobject.java
+++ src/main/java/nl/han/ica/StarDestroyer/Textobject.java
@@ -4,4 +4,27 @@ package nl.han.ica.StarDestroyer;
  * Created by Marijn Besseling on 24-Mar-17.
  */
 public class Textobject {
+    private String text;
+    private int score;
+
+    public Textobject(String text, int score) {
+        this.text = text;
+        this.score = score;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
