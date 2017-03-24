ade01e9f0b3d014cccb702c1719cba8148ac8a0c
Marijn Besselking <njirambem@gmail.com>
2017-03-24 09:45:39 +0100

    renamed MainApp to GameApp

--- src/main/java/nl/han/ica/StarDestroyer/GameApp.java
+++ src/main/java/nl/han/ica/StarDestroyer/GameApp.java
@@ -6,9 +6,9 @@ import processing.core.PApplet;
 /**
  * Created by Marijn Besseling on 22-Mar-17.
  */
-public class MainApp extends GameEngine {
+public class GameApp extends GameEngine {
     public static void main(String[] args) {
-        PApplet.main(new String[]{"nl.han.ica.StarDestroyer.MainApp"});
+        PApplet.main(new String[]{"nl.han.ica.StarDestroyer.GameApp"});
     }
 
     public void setupGame() {
