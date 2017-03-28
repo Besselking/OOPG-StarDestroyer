46c152f0fb20a2f69d661b12811e994d0a3906cc
Marijn Besselking <njirambem@gmail.com>
2017-03-28 15:01:53 +0200

    player movement begin

--- src/main/java/nl/han/ica/StarDestroyer/Player.java
+++ src/main/java/nl/han/ica/StarDestroyer/Player.java
@@ -14,19 +14,35 @@ public class Player extends GameObject implements ICollidableWithGameObjects {
     private int shots;
     private IPowerup PU;
     private GameApp app;
+    private float direction = getDirection();
 
     public Player(GameApp app) {
         this.app = app;
         this.life = 3;
         this.shots = 1;
+        setDirection(0);
     }
 
     @Override
     public void update() {
+        setDirection(direction += 0.01);
     }
 
     @Override
     public void draw(PGraphics g) {
+        g.translate(super.x, super.y);
+        g.rotate(getDirection() - 90);
+        g.fill(255);
+        g.stroke(255);
+        g.rectMode(CENTER);
+        g.rect(0, 0, 20, 20);
+        g.line(-10, 10, -10, 20);
+        g.line(10, 10, 10, 20);
+        g.fill(255, 128, 0);
+        g.noStroke();
+        g.triangle(-8, 11, 8, 11, 0, 30);
+        g.rotate(-getDirection());
+        g.translate(0, 0);
     }
 
 
